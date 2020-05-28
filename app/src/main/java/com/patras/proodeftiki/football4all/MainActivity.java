59c4807fca13f884febfe1de346cde7eb00cbbe1
johnisht <iwannispap2@gmail.com>
2020-05-28 19:53:15 +0300

    Updated 17/04 based on the new Domain Model

--- app/src/main/java/com/patras/proodeftiki/football4all/MainActivity.java
+++ app/src/main/java/com/patras/proodeftiki/football4all/MainActivity.java
@@ -44,22 +44,90 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public class User{
+    public class User {
+        public void inchange(int game,int comm_id) {
 
+        }
+        public void grandPermission(int comm_id){
+
+        }
+        public void displayForm(){
+
+        }
+        public void isFirstTime(){
+
+        }
+        public void displayAndGiveID(){
+
+        }
+        public void searchID(){
+
+        }
+        public void displayUser(){
+
+        }
+        public void isFilled(){
+
+        }
+        public void sendToAdmin(){
+
+        }
+        public void displayEditAcc(){
+
+        }
+        public void delete(){
+
+        }
+        public void isLoggedIn(){
+
+        }
     }
 
     public class Viewer extends User{
-
+        private int t_id;
     }
 
     public class dis_Viewer extends User{
-
+        private int t_id;
     }
 
     public class Reporter{
         int d_int=0;
         String name,surname;
         Date date = new Date();
+        public void create_acc(){
+
+        }
+        public void displayF(){
+
+        }
+        public void check(){
+
+        }
+        public void soc_med_con(){
+
+        }
+        public void save(){
+
+        }
+        public void media_select(){
+
+        }
+        public void display_C(){
+
+        }
+        public void approval(){
+
+        }
+        public void displayA(){
+
+        }
+        public void importData(){
+
+        }
+        public void displayW(){
+
+        }
     }
 
     public class Report_for_Dis extends Reporter{
@@ -67,32 +135,103 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class Voice_assistant{
+        public void sound_message(){
 
+        }
+        public void recordMessage(){
+
+        }
+        public void sendToAdmin(){
+
+        }
+        public void voice_command_rec(){
+
+        }
+        public void optic_to_text(){
+
+        }
+        public void new_info(){
+
+        }
+        public void optic_to_sound(){
+
+        }
+        public void voice_command(){
+
+        }
+        public void ready_for_service(){
+
+        }
+        public void update_to_base(){
+
+        }
+        public void help(){
+
+        }
     }
 
     public class Match{
         int a_id,start_time,score;
+        public void start(){
+
+        }
+        public void stop(){
+
+        }
+        public void displayGInstert(){
+
+        }
+        public void score(){
+
+        }
+        public void displayGame(){
+
+        }
     }
 
     public class Player{
         String name,position;
         int age,number;
+
+        public void Modify_Player(){
+
+        }
+        public void Delete_Player(){
+
+        }
+        public void display_selectPlayer(){
+
+        }
+        public void display_PlayerStats(){
+
+        }
     }
 
     public class Line_Up extends Player{
+        public void Modify_Team(){
 
+        }
+        public void Delete_Team(){
+
+        }
+        public void display_teamSelect(){
+
+        }
+        public void display_TeamStats(){
+
+        }
     }
 
     public class Statistics{
-        int corner,offside,fouls,cards;
+        private int corner,offside,fouls,cards;
     }
 
     public class Match_Statistics extends Statistics{
-
+        private int possession;
     }
 
     public class Player_Statistics extends Statistics{
-
+        private int goals, fouls_committed,shoots,mistakes;
     }
 
     public class Commentary{
@@ -100,12 +239,154 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class Ball{
-        float position_x,position_y;
+        private float position_x,position_y;
     }
 
     public class Database{
+        public void get_info(int par){
 
+        }
+        public void check_if_available(){
+
+        }
+        public void displayGameCovered(){
+
+        }
+        public void displayToVI(){
+
+        }
+        public void displayToNonVI(){
+
+        }
+        public void displayNoGames(){
+
+        }
+        public void displayGames(){
+
+        }
+        public void search(int no_coverage){
+
+        }
+        public void displayNotCovered(){
+
+        }
+        public void SaveUserSelection(){
+
+        }
+        public void successSave(){
+
+        }
     }
 
+    public class LiveShow{
+        public void display(){
+
+        }
+        public void isApproved(){
+
+        }
+        public void isMachineReadyForLive(){
+
+        }
+        public void startLive(){
+
+        }
+        public void notifyUsers(){
+
+        }
+        public void existLive(){
+
+        }
+        public void categoryOfUser(){
+
+        }
+        public void displayAllLives(){
+
+        }
+        public void displayLive(){
+
+        }
+    }
+
+    public class News{
+        public void checkSize(){
+
+        }
+        public void displayFiles(){
+
+        }
+        public void isSafe(){
+
+        }
+        public void display(){
+
+        }
+        public void isApprove(){
+
+        }
+        public void publishArticle(){
+
+        }
+        public void existNews(){
+
+        }
+        public void displayArticle(){
+
+        }
+    }
+
+    public class Chat{
+        public void displayChat(){
+
+        }
+        public void use(){
+
+        }
+        public void checkMessage(){
+
+        }
+        public void sendMessage(){
+
+        }
+    }
+
+    public class VoiceChat extends Chat{
+        public void record(){
+
+        }
+        public void displaySend(){
+
+        }
+        public void store(){
+
+        }
+        public void play(String message){
+
+        }
+        public void search(String message){
+
+        }
+        public void activate(){
+
+        }
+        public void displayPlay(){
+
+        }
+        public void displayToUser(){
+
+        }
+    }
+
+    public class History{
+        public void displayEditHistory(){
+
+        }
+        public void Modify_History(){
+
+        }
+        public void displayHistoryLeague(){
+
+        }
+    }
 
 }
